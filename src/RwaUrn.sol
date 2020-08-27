3c72b5d65817e01a2e2d01a869479c0890c63756
Lev Livnev <lev@liv.nev.org.uk>
2020-08-27 11:38:49 +0000

    RwaUrn.sol: separate wards and operators

--- src/RwaUrn.sol
+++ src/RwaUrn.sol
@@ -19,10 +19,17 @@ contract JoinLike {
 contract RwaUrn {
     // --- auth ---
     mapping (address => uint) public wards;
+    mapping (address => uint) public can;
     function rely(address usr) external auth { wards[usr] = 1; }
     function deny(address usr) external auth { wards[usr] = 0; }
     modifier auth {
         require(wards[msg.sender] == 1, "RwaUrn/not-authorized");
+        _;
+    }
+    function hope(address usr) external auth { can[usr] = 1; }
+    function nope(address usr) external auth { can[usr] = 0; }
+    modifier operator {
+        require(can[msg.sender] == 1, "RwaUrn/not-operator");
         _;
     }
 
@@ -42,21 +49,21 @@ contract RwaUrn {
 
     // --- cdp operation ---
     // n.b. DAI can only go to fbo
-    function lock(uint256 wad) external auth {
+    function lock(uint256 wad) external operator {
         gemJoin.gem().transferFrom(msg.sender, address(this), wad);
         gemJoin.join(address(this), wad);
         vat.frob(gemJoin.ilk(), address(this), address(this), address(this), int(wad), 0);
     }
-    function free(uint256 wad) external auth {
+    function free(uint256 wad) external operator {
         vat.frob(gemJoin.ilk(), address(this), address(this), address(this), -int(wad), 0);
         gemJoin.exit(address(this), wad);
         gemJoin.gem().transfer(msg.sender, wad);
     }
-    function draw(uint256 wad) external auth {
+    function draw(uint256 wad) external operator {
         vat.frob(gemJoin.ilk(), address(this), address(this), address(this), 0, int(wad));
         daiJoin.exit(fbo, wad);
     }
-    function wipe(uint256 wad) external auth {
+    function wipe(uint256 wad) external operator {
         vat.frob(gemJoin.ilk(), address(this), address(this), address(this), 0, -int(wad));
     }
 }
