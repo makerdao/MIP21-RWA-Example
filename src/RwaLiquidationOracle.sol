3eb10f227e9a70f95fe9af27dbc4fc17234f5857
Julien Martin <julienmartinlevrai@protonmail.ch>
2022-02-01 23:18:12 -0500

    docs: add license identifier to oracle

--- src/RwaLiquidationOracle.sol
+++ src/RwaLiquidationOracle.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+//
 // Copyright (C) 2020, 2021 Lev Livnev <lev@liv.nev.org.uk>
 //
 // This program is free software: you can redistribute it and/or modify
